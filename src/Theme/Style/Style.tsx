5d040ff74cffd00d6aba2113bfbd5af1d2a03d4a
Stiven Trujillo <luis.trujillo@hibot.us>
2021-11-28 01:02:36 -0500

    Update Global Style

--- src/Theme/Style/Style.tsx
+++ src/Theme/Style/Style.tsx
@@ -4,6 +4,11 @@ import { normalize } from './normalize'
 const GlobalStyle = createGlobalStyle`
     ${normalize}
 
+    *, *:before, *:after {
+        box-sizing: inherit;
+        margin: 0;
+    }
+
     body {
         font-family: 'Grenze Gotisch', cursive;
         font-weight: 400; 
