d8c6f8c05b5d9df447206bdba0c4a6627c3ac006
Stiven Trujillo <luis.trujillo@hibot.us>
2021-11-27 23:10:22 -0500

    Example how to use style components with theme and typescript

--- src/TestsApp.tsx
+++ src/TestsApp.tsx
@@ -1,5 +1,43 @@
 import React from 'react'
 import { Theme } from './Theme'
+import styled, { css } from 'styled-components'
+
+// STYLED COMPONENTS
+
+// Ejemplo código css dinámico
+const flex = css`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+`
+
+// Definir atributos por defecto para los styled components
+const Input = styled.input.attrs( props => ({
+    placeholder: props.placeholder || 'Type something',
+    type: props.type || 'text'
+}))`
+    padding: 1em;
+    height: 27px;
+`
+
+// Custom Style Components Props with TypeScript
+interface ButtonProps {
+    readonly bg?: string
+}
+const Button = styled.button<ButtonProps>`
+    padding: 1em 2.5em;
+    margin: 1em;
+    background-color: ${ props => props.bg || props.theme.colors.gray};
+    /*border: none; */
+`
+// También se puede hacer de la siguiente forma: (Lo malo es que el snippet no funcionaría)
+/*const Button2 = styled.button<ButtonProps>(props => `
+    padding: 1em 2.5em;
+    margin: 1em;
+    background-color: ${ props.bg || ''}
+`)*/
+
 
 // Custom Props with TypeScript
 interface AppProps {
@@ -9,7 +47,9 @@ interface AppProps {
 const TestsApp: React.FC<AppProps> = (props) => {
     return (
         <Theme>
-            <div>hello world</div>
+            <p>hello world</p>
+            <Input />
+            <Button bg="lightblue">hola</Button>
         </Theme>
     )
 }
