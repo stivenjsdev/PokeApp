e12e805aba87533590600d8c31e080a1802019e2
Stiven Trujillo <luis.trujillo@hibot.us>
2021-12-02 17:03:00 -0500

    Update PokemonCard Types

--- src/components/common/PokemonCard/PokemonCard.type.ts
+++ src/components/common/PokemonCard/PokemonCard.type.ts
@@ -1,5 +1,6 @@
+import { Card } from '../../shared/Card/Card.type'
 
-export interface PokemonCard {
+export interface PokemonCard extends Card {
     readonly image?: string;
     readonly name?: string;
     readonly types?: string;
