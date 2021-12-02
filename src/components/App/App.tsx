3b89037897ab118f64455d3bc2829d52c4a54ae6
Stiven Trujillo <luis.trujillo@hibot.us>
2021-12-02 17:03:53 -0500

    Update App Component, and Add styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import { Theme } from '../../Theme'
-import { Header } from '../common/Header'
+import { PokemonCard } from '../common/PokemonCard'
+import * as Styled from './App.styled'
 
 const App = () => {
   return (
     <Theme>
-      <Header />
+      <Styled.App>
+        <Styled.Header />
+
+        <Styled.Sidebar>
+          <PokemonCard
+            image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+            name="pikachu"
+            types="electric roedor"
+            bgColor="#F5F8FA"
+          />
+        </Styled.Sidebar>
+
+        <Styled.StyledMain>
+          
+        </Styled.StyledMain>
+      </Styled.App>
     </Theme>
   )
 }
