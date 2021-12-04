0fe20889ecb576e5c961f654c45d217cd2180272
Stiven Trujillo <luis.trujillo@hibot.us>
2021-12-04 09:01:19 -0500

    Delete TestsApp component

--- src/TestsApp.tsx
+++ src/TestsApp.tsx
@@ -1,22 +1,15 @@
 import React from 'react'
 import { Theme } from './Theme'
-import { Header } from './components/common/Header'
-import { PokemonCard } from './components/common/PokemonCard'
 
 // Custom Props with TypeScript
 interface AppProps {
     readonly message?: string // readonly=const ?=optional
 }
 
-const TestsApp: React.FC<AppProps> = (props) => {
+const TestsApp = (props: AppProps) => {
     return (
         <Theme>
-            {/* <Header /> */}
-            <PokemonCard 
-                image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" 
-                name="pikachu"
-                types="electric roedor"
-            />
+            
         </Theme>
     )
 }
