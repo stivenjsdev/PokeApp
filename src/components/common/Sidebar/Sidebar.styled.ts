e51e9e344634a7feba1bd144bf99875bed9e639c
Stiven Trujillo <luis.trujillo@hibot.us>
2021-12-04 08:58:49 -0500

    Update Sidebar

--- src/components/common/Sidebar/Sidebar.styled.ts
+++ src/components/common/Sidebar/Sidebar.styled.ts
@@ -1,10 +1,21 @@
 import styled from 'styled-components'
+import { Button as SimpleButton } from '../../shared/Button'
 
 export const Sidebar = styled.aside`
     display: flex;
-    justify-content: center;
-    align-items: flex-start;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
     
     background-color: ${props => props.theme.colors.white};
     padding: 10px;
+`
+
+export const Message = styled.p`
+    margin: 5px 0;
+`
+
+export const Button = styled(SimpleButton)`
+    margin: 5px 0;
+    border-radius:0.12em;
 `
