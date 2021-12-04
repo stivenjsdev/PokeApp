44ec48feb0e593dc1109b204b2c0c6630c499e27
Stiven Trujillo <luis.trujillo@hibot.us>
2021-12-04 08:57:10 -0500

    header doesnt receives pokemon

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -3,12 +3,11 @@ import * as Styled from './Header.styled'
 import * as Types from './Header.type'
 import { Search } from '../../shared/Search'
 
-export const Header = ({getPokemon, ...properties}: Types.Header) => {
+export const Header = ({...properties}: Types.Header) => {
     return (
         <Styled.Header {...properties}>
             <Styled.Title>Poke APP</Styled.Title>
             <Search
-                getPokemon={ getPokemon }
                 placeholder="Search a Pokemon..." 
                 bgColor="#F5F8FA"
             />
