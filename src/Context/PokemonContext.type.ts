da9a5f8c75c48429450a9960d6aa0b003a7853f4
Stiven Trujillo <luis.trujillo@hibot.us>
2021-12-06 22:46:16 -0500

    Update Context

--- src/Context/PokemonContext.type.ts
+++ src/Context/PokemonContext.type.ts
@@ -3,8 +3,14 @@ import { IPokemon } from "../types/IPokemon";
 export interface PokemonContext {
     pokemon: IPokemon | undefined;
     searchPokemon: (pokemonName: string | number) => void;
-    loading: boolean;
-    error: boolean;
+    searchLoading: boolean;
+    searchError: boolean;
+    pokeTeam: IPokemon[];
+    savePokeTeam: (newItem: IPokemon[]) => void;
+    saveLoading: boolean;
+    saveError: boolean
+    catchPokemon: (pokemon: IPokemon) => void;
+    deletePokemon: (pokemonId: number) => void;
 }
 
 export interface PokemonProvider {
